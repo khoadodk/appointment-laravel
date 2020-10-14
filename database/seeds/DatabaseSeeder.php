e493d49f8786320315b79abddd9fb187716b4c35
Khoa Do <khoado.dk@gmail.com>
2020-10-14 14:10:04 -0400

    Added admin seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\User;
 use App\Role;
 
 class DatabaseSeeder extends Seeder
@@ -13,8 +16,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
         Role::create(['name' => 'doctor']);
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'patient']);
+
+        $admin = new User([
+            "name" => 'Admin',
+            "email" => 'admin@gmail.com',
+            "email_verified_at" => now(),
+            'password' => Hash::make('password'),
+            'role_id' => 2,
+            'gender' => 'male',
+            'remember_token' => Str::random('10'),
+        ]);
+
+        $admin->save();
     }
 }
