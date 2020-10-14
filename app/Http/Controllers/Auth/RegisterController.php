09628a989ec170ff8a04a696a1e3eef497d38684
Khoa Do <khoado.dk@gmail.com>
2020-10-14 13:02:14 -0400

    Fix the role_id in register Controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,12 +66,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $role = Role::where('name', 'patient')->first();
+        // $role = Role::where('name', 'patient')->first();
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role_id' => $role->id,
+            // role_id is patient
+            'role_id' => 3,
             'gender' => $data['gender']
         ]);
     }
